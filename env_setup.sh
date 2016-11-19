0ea72ecf3d42904b2b04f70f0a5db0882031086e
angelboy <scwuaptx@gmail.com>
2016-11-19 14:14:45 +0800

    Add env setup script

---
+++ env_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+#
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install binutils nasm
+sudo apt-get -y install gcc-multilib g++-multilib
+sudo apt-get -y install libc6-dev-i386
+sudo apt-get -y install git
+sudo apt-get -y install libc6-dbg libc6-dbg:i386
+sudo apt-get -y install nmap
+sudo apt-get -y install python-pip libssl-dev
+sudo apt-get -y install gdb
+sudo pip install --upgrade pip
+sudo pip install --upgrade capstone
+sudo pip install --upgrade pwntools
+sudo pip install ropgadget
+git clone https://github.com/scwuaptx/peda.git ~/peda 
+cp ~/peda/.inpurc ~/
+git clone https://github.com/scwuaptx/Pwngdb.git
+cp ~/Pwngdb/.gdbinit ~/
+git clone https://github.com/JonathanSalwan/ROPgadget
